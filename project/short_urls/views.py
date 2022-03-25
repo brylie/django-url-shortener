f74c7cfcd06d29e9a032eea211c8ee4ce3f197a9
Brylie Christopher Oxley <brylie@amble.fi>
2022-03-25 12:03:24 +0200

    Initial ShortUrl create, delete, detail, and redirect views

--- project/short_urls/views.py
+++ project/short_urls/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
+from django.views import View
+from django.views.decorators.http import require_http_methods
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView
 
-# Create your views here.
+from .models import ShortUrl
+
+
+class ShortUrlCreateView(CreateView):
+    model = ShortUrl
+    fields = ['redirect_url']
+
+    def get_success_url(self):
+        print("short url success url")
+        return reverse('short-url-detail', kwargs={'pk' : self.object.pk})
+
+
+class ShortUrlDeleteView(DeleteView):
+    model = ShortUrl
+    success_url = reverse_lazy('short-url-create')
+
+
+class ShortUrlDetailView(DetailView):
+    model = ShortUrl
+
+
+class ShortUrlRedirectView(View):
+    def get(self, request, pk):
+        short_url = ShortUrl.objects.get(pk=pk)
+
+        return redirect(short_url.redirect_url)
