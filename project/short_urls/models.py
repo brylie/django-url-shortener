1cb76c7a51ed84f5c7c37abc14ddb6e12bbf2c33
Brylie Christopher Oxley <brylie@amble.fi>
2022-03-25 18:13:08 +0200

    Add ShortURL.slug field

--- project/short_urls/models.py
+++ project/short_urls/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 
 class ShortUrl(models.Model):
     redirect_url = models.URLField()
+    slug = models.SlugField(null=True)
+
 
     def get_absolute_url(self):
         return reverse('short-url-detail', kwargs={'pk': self.pk})
