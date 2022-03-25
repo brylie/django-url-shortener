03dd3611ec2b701b5abff1c960a105a0f3eabaf9
Brylie Christopher Oxley <brylie@amble.fi>
2022-03-25 18:13:46 +0200

    Use slug field instead of pk

--- project/short_urls/urls.py
+++ project/short_urls/urls.py
@@ -4,9 +4,7 @@ from .views import ShortUrlCreateView, ShortUrlDeleteView, ShortUrlDetailView, S
 
 urlpatterns = [
     path('', ShortUrlCreateView.as_view(), name='short-url-create'),
-    # path('<slug:slug>/delete', ShortUrlDeleteView.as_view(), name='short-url-delete'),
-    # path('<slug:slug>/detail', ShortUrlDetailView.as_view(), name='short-url-detail'),
-    path('<pk>/delete', ShortUrlDeleteView.as_view(), name='short-url-delete'),
-    path('<pk>/detail', ShortUrlDetailView.as_view(), name='short-url-detail'),
-    path('<pk>', ShortUrlRedirectView.as_view(), name='short-url-redirect'),
+    path('<slug:slug>/delete', ShortUrlDeleteView.as_view(), name='short-url-delete'),
+    path('<slug:slug>/detail', ShortUrlDetailView.as_view(), name='short-url-detail'),
+    path('<slug>', ShortUrlRedirectView.as_view(), name='short-url-redirect'),
 ]
