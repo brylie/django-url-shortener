316599e10db3e73efa9b9cfd9c562c68f2f01f4d
Brylie Christopher Oxley <brylie@amble.fi>
2022-03-25 12:02:51 +0200

    Initial ShortUrl model with get_absolute_url

--- project/short_urls/models.py
+++ project/short_urls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class ShortUrl(models.Model):
+    redirect_url = models.URLField()
+
+    def get_absolute_url(self):
+        return reverse('short-url-detail', kwargs={'pk': self.pk})
