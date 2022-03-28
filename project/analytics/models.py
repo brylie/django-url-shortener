e5915448d9444bad2ee8bfc686bab7591aac7c4b
Brylie Christopher Oxley <brylie@amble.fi>
2022-03-28 10:33:27 +0300

    Initial ShortUrlVisit model/index

--- project/analytics/models.py
+++ project/analytics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from short_urls.models import ShortUrl
+
+
+class ShortUrlVisit(models.Model):
+    short_url = models.ForeignKey(to=ShortUrl, on_delete=models.CASCADE)
+    occurred = models.DateTimeField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["occurred"])
+        ]
