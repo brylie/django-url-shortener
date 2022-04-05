8990c02444c702ac88c3acb4d7880b8624d6d6ad
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-05 17:14:53 +0300

    Move ShortUrlTestCase to correct app

--- analytics/tests.py
+++ analytics/tests.py
@@ -1,13 +1,1 @@
 from django.test import TestCase
-
-from .models import ShortUrl
-
-class ShortUrlTestCase(TestCase):
-    def setUp(self):
-        ShortUrl.objects.create(redirect_url="https://test.com")
-
-    def test_short_url_has_automatic_slug(self):
-        """Make sure short URLs have auto-generated slug"""
-        short_url = ShortUrl.objects.get(id=1)
-
-        self.assertIsNotNone(short_url.slug)
