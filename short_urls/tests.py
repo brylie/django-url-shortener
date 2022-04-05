4aaccc8485ad1fa867bfce110c1d6b392ca96b8e
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-05 17:22:12 +0300

    Add ShortUrlDetailViewTest analytics list

--- short_urls/tests.py
+++ short_urls/tests.py
@@ -1,6 +1,9 @@
-from django.test import TestCase
+from typing import List
+from django.test import RequestFactory, TestCase
+from django.urls import reverse
 
 from .models import ShortUrl
+from .views import ShortUrlDetailView
 
 class ShortUrlTestCase(TestCase):
     def setUp(self):
@@ -11,3 +14,26 @@ class ShortUrlTestCase(TestCase):
         short_url = ShortUrl.objects.get(id=1)
 
         self.assertIsNotNone(short_url.slug)
+
+
+class ShortUrlDetailViewTest(TestCase):
+    def setUp(self):
+        # Create a testing short URL
+        self.short_url = ShortUrl.objects.create(
+            redirect_url="https://google.com",
+        )
+
+    def test_analytics_list_in_context(self):
+        """Short URL detail view should have analytics list in context"""
+        short_url_url = reverse(
+            "short-url-detail",
+            kwargs={
+                "slug": self.short_url.slug,
+            }
+        )
+        response = self.client.get(short_url_url)
+        
+        context = response.context
+
+        self.assertIn("analytics", context)
+        self.assertIsInstance(context["analytics"], List)
