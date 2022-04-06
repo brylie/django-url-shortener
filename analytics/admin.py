3d5ef9e6b644452a1af9ac20189aa01820ecb3d4
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-06 13:32:22 +0300

    Register ShortUrlVisitAdmin

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ShortUrlVisit
+
+
+@admin.register(ShortUrlVisit)
+class ShortUrlVisitAdmin(admin.ModelAdmin):
+    list_display = ('short_url', 'occurred',)
